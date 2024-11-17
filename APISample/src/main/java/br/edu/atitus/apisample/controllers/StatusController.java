00a5aec055a4495c9a7cb5ad69b8ca1971b7e5b4
gabrielgrasik <gabrielgrasik@gmail.com>
2024-11-16 23:04:47 -0300

    mudança git

--- APISample/src/main/java/br/edu/atitus/apisample/controllers/StatusController.java
+++ APISample/src/main/java/br/edu/atitus/apisample/controllers/StatusController.java
@@ -1,17 +1,19 @@
 package br.edu.atitus.apisample.controllers;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/status")
 public class StatusController {
 	
-	@GetMapping({"/", ""})
+	@GetMapping({"","/"})
 	public ResponseEntity<String> getStatus() {
-		return ResponseEntity.ok("Server is runing ... ");
+		return ResponseEntity.ok("Server is runing...");
 	}
+	
 
 }
